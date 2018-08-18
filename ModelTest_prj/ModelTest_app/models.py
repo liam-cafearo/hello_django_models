8755e01ff8342d3e03a1679a3c81f93cbcae2be3
Liam-Cafearo <lcafearo@gmail.com>
2018-08-18 18:26:47 +0100

    initial commit

---
+++ ModelTest_prj/ModelTest_app/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+
+    class Meta:  # include this to ensure build in IDE
+        app_label = "ModelTest_app"
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=20)
